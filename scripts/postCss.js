09ee22cde8f99198697e792bfdf7c3a6036e017e
Dale Sande <dale.sande@gmail.com>
2023-10-09 20:14:00 -0700

    build: remove unused style resource
    This commit also addresses an error where the CSS resources were not
    being processed through the PostCSS script.
    
    Changes to be committed:
    modified:   scripts/postCss.js
    deleted:    src/style.scss

--- scripts/postCss.js
+++ scripts/postCss.js
@@ -3,16 +3,30 @@ const postcss = require('postcss');
 const comments = require('postcss-discard-comments');
 const fs = require('fs');
 
-fs.readFile('src/style.css', (err, css) => {
+fs.readFile('src/auro-checkbox-group.css', (err, css) => {
   postcss([autoprefixer, comments])
     .use(comments({
       remove: function(comment) { return comment[0] == "@"; }
     }))
-    .process(css, { from: 'src/style.css', to: 'src/style.css' })
+    .process(css, { from: 'src/auro-checkbox-group.css', to: 'src/auro-checkbox-group.css' })
     .then(result => {
-      fs.writeFile('src/style.css', result.css, () => true)
+      fs.writeFile('src/auro-checkbox-group.css', result.css, () => true)
       if ( result.map ) {
-        fs.writeFile('src/style.map', result.map, () => true)
+        fs.writeFile('src/checkbox.map', result.map, () => true)
+      }
+    })
+});
+
+fs.readFile('src/auro-checkbox-group.css', (err, css) => {
+  postcss([autoprefixer, comments])
+    .use(comments({
+      remove: function(comment) { return comment[0] == "@"; }
+    }))
+    .process(css, { from: 'src/auro-checkbox-group.css', to: 'src/auro-checkbox-group.css' })
+    .then(result => {
+      fs.writeFile('src/auro-checkbox-group.css', result.css, () => true)
+      if ( result.map ) {
+        fs.writeFile('src/checkbox-group.map', result.map, () => true)
       }
     })
 });
