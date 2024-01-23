0e35eb73408e5078b1cb2d10eb0e3284164064cc
Dale Sande <dale.sande@gmail.com>
2024-01-22 18:04:07 -0800

    build: update postCss to use loop script
    This commit addresses a slight miss where not all style scripts were
    going through the postCSS script. No CSS output errors were caused
    because of this, but needed to be corrected.
    
    Changes to be committed:
    modified:   scripts/postCss.mjs

--- scripts/postCss.mjs
+++ scripts/postCss.mjs
@@ -1,32 +1,41 @@
 import autoprefixer from 'autoprefixer';
 import postcss from 'postcss';
 import comments from 'postcss-discard-comments';
+import path from 'path';
 import fs from 'fs';
+const __dirname = new URL('.', import.meta.url).pathname;
+const directoryPath = path.join(__dirname, '../src');
 
-fs.readFile('src/auro-checkbox-group.css', (err, css) => {
-  postcss([autoprefixer, comments])
-    .use(comments({
-      remove: function(comment) { return comment[0] == "@"; }
-    }))
-    .process(css, { from: 'src/auro-checkbox-group.css', to: 'src/auro-checkbox-group.css' })
-    .then(result => {
-      fs.writeFile('src/auro-checkbox-group.css', result.css, () => true)
-      if ( result.map ) {
-        fs.writeFile('src/checkbox.map', result.map, () => true)
-      }
-    })
+/**
+ * Default postCSS run
+ */
+fs.readdir(directoryPath, function (err, files) {
+  //handling error
+  if (err) {
+      return console.log('Unable to scan directory: ' + err);
+  }
+  //listing all files using forEach
+  files.forEach(function (file) {
+    if (file.includes(".css")) {
+      standardProcessor(file);
+    }
+  });
 });
 
-fs.readFile('src/auro-checkbox-group.css', (err, css) => {
-  postcss([autoprefixer, comments])
-    .use(comments({
-      remove: function(comment) { return comment[0] == "@"; }
-    }))
-    .process(css, { from: 'src/auro-checkbox-group.css', to: 'src/auro-checkbox-group.css' })
-    .then(result => {
-      fs.writeFile('src/auro-checkbox-group.css', result.css, () => true)
-      if ( result.map ) {
-        fs.writeFile('src/checkbox-group.map', result.map, () => true)
-      }
-    })
-});
+/**
+ * The standardProcessor function applies tokens for fallback selectors
+ * and completes a post cleanup.
+ * @param {string} file
+ */
+function standardProcessor(file) {
+ fs.readFile(`src/${file}`, (err, css) => {
+   postcss([autoprefixer, comments])
+   .use(comments({
+     remove: function(comment) { return comment[0] == "@"; }
+   }))
+   .process(css, { from: `src/${file}`, to: `src/${file}` })
+   .then(result => {
+     fs.writeFile(`src/${file}`, result.css, () => true)
+   })
+ });
+}
