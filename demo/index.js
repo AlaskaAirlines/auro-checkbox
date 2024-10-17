69400851315e73d76ae289aae5305c9ef51f86ea
Eunsun Mota <Eunsun.mota@alaskaair.com>
2024-10-17 10:24:01 -0700

    feat(api): add register static method
    AuroCheckbox.register is to easily register the element without extra importing
    `import '@aurodesignsystem/auro-checkbox'` will still register this element to `<auro-checkbox>`
    `import { AuroCheckbox } from '../src/auro-checkbox` wont register this element until `AuroCheckbox.register` gets called
    
    Same changes applied to AuroCheckboxGroup

---
+++ demo/index.js
@@ -0,0 +1,5 @@
+import { AuroCheckbox } from '../src/auro-checkbox.js';
+import { AuroCheckboxGroup } from '../src/auro-checkbox-group.js';
+
+AuroCheckbox.register('custom-checkbox');
+AuroCheckboxGroup.register('custom-checkbox-group');
